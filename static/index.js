8eb3a8a933d798be670ea49d517ccb5ca83ef574
Andy <andyqin17@gmail.com>
2021-07-11 21:37:40 -0700

    Improve frontend view

--- static/index.js
+++ static/index.js
@@ -2,65 +2,65 @@ window.addEventListener("load", init);
 
 function init() {
   console.log("Window loaded!");
-  document.getElementById("file-input").addEventListener("change", handleFileChange);
-  document.getElementById("image-upload").addEventListener("submit", handleSubmit);
-
-  fetch("/svd/example")
-    .then(statusCheck)
-    .then(res => res.json())
-    .then((json) => renderRGBOnCanvas(json.colors, json.shape[0], json.shape[1]))
-    .catch(console.log);
+  id("upload-btn").disabled = true;
+  id("file-input").addEventListener("change", handleFileChange);
+  id("image-upload").addEventListener("submit", handleSubmit);
+  // fetch("/svd/example")
+  //   .then(statusCheck)
+  //   .then(res => res.json())
+  //   .then((json) => renderRGBOnCanvas(json.colors, json.shape[0], json.shape[1]))
+  //   .catch(console.log);
 }
 
 function handleSubmit(e) {
   e.preventDefault();
-  let file = document.getElementById("file-input").files[0];
+  id("upload-btn").disabled = true;
+  id("upload-spinner").classList.remove("d-none");
+  let file = id("file-input").files[0];
   if (file) {
+    let canvas = id("canvas");
+    let ctx = canvas.getContext("2d");
+    let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
     let formData = new FormData();
-    console.log(file);
-    formData.append("image", file, file.name);
-    formData.append("name", "Kibby");
+    formData.append("data", imgData.data);
+    formData.append("width", canvas.width);
+    formData.append("height", canvas.height);
+
     fetch('/upload/image', { method: "POST", body: formData })
-      .then(console.log);
-    console.log("Form submitted!");
+      .then(statusCheck)
+      .then(res => res.json())
+      .then((json) => renderRGBOnCanvas(json.colors, json.shape[0], json.shape[1]))
+      .catch(console.log)
+      .finally(() => id("upload-spinner").classList.add("d-none"))
   } else {
     console.log("Missing file!");
   }
 }
 
 function handleFileChange(e) {
-  let label = document.getElementById("file-label");
+  let label = id("file-label");
   if (this.files.length >= 1) {
+    id("upload-btn").disabled = false;
     label.textContent = this.files[0].name;
     let img = new Image();
     img.src = URL.createObjectURL(this.files[0]);
     img.addEventListener("load", () => renderImageOnCanvas(img));
   } else {
+    id("upload-btn").disabled = true;
     label.textContet = "Choose file";
   }
 }
 
 function renderImageOnCanvas(img) {
-  let canvas = document.getElementById("canvas");
+  let canvas = id("canvas");
   let ctx = canvas.getContext("2d");
   canvas.width = img.width;
   canvas.height = img.height;
   ctx.drawImage(img, 0, 0);
-  let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-
-  let formData = new FormData();
-  formData.append("data", imgData.data);
-  formData.append("width", canvas.width);
-  formData.append("height", canvas.height);
-  fetch('/upload/image', { method: "POST", body: formData })
-    .then(statusCheck)
-    .then(res => res.json())
-    .then((json) => renderRGBOnCanvas(json.colors, json.shape[0], json.shape[1]))
-    .catch(console.log)
 }
 
 function renderRGBOnCanvas(rgb, height, width) {
-  let canvas = document.getElementById("canvas");
+  let canvas = id("canvas");
   let ctx = canvas.getContext("2d");
   ctx.canvas.width = width;
   ctx.canvas.height = height;
@@ -85,4 +85,8 @@ async function statusCheck(response) {
     throw new Error(await response.text());
   }
   return response;
+}
+
+function id(element) {
+  return document.getElementById(element);
 }
