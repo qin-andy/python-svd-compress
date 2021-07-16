efbfecb7474e2d3ea0f1bb10cb79b79872eb6dbf
Andy <andyqin17@gmail.com>
2021-07-15 17:31:56 -0700

    Add redis

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
+import time
+
 from PIL import Image
 from flask import Flask, render_template, request, session
+import redis
 from markupsafe import escape
 from ImageSVD import ImageSVD
 
 
 app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
 secret_file = open("secret_key.txt");
 app.secret_key = secret_file.read()
 
@@ -14,6 +18,17 @@ examples = [
     ImageSVD("static/images/horizon.png"),
     ImageSVD("static/images/shore.png")
 ]
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
 
 
 @app.route("/")
@@ -72,3 +87,8 @@ def count():
         session['count'] = 0
     session['count'] += 1
     return str(session['count'])
+
+@app.route("/redis/hits")
+def hits():
+    count = get_hit_count()
+    return 'Hello World! I have been seen {} times.\n'.format(count)
